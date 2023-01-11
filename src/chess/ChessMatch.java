1d9e217190b36bbf2819a82900d5ff291c7e9e33
Igor Bondezam França <igorbfraca@hotmail.com>
2023-01-11 19:38:58 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -83,7 +83,8 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece captiredPiece = board.removePiece(target);
         board.placePiece(p, target);
 
@@ -98,7 +99,8 @@ public class ChessMatch {
 
 
     private void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.increaseMoveCount();
         board.placePiece(p, source);
 
         if (capturedPiece != null) {
